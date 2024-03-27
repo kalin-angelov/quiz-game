9234bc6a4a6597cbf000ec138105e511fb3104d9
Kalizar <kalinangelov@abv.bg>
2024-03-27 10:51:29 +0200

    Create Card True

---
+++ quiz/src/components/CardTrue/CardTrue.jsx
@@ -0,0 +1,11 @@
+import styles from './CardTrue.module.css';
+
+export const CardTrue = () => {
+    return (
+        <div className={styles.card}>
+            <div className={styles.cardFront}>
+                <img src="images/card-front-true.png" alt="card-front-true" className={styles.cardImg} />
+            </div>
+        </div>
+    );
+};
