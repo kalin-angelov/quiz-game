fd90a7571bf556ac0a494bb68031707669dd0ce2
Kalizar <kalinangelov@abv.bg>
2024-03-27 10:52:20 +0200

    Create Footer

---
+++ quiz/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+export const Footer = () => {
+    return (
+        <footer>
+            <div className={styles.footer}>
+                <p>Good Luck</p>
+            </div>
+        </footer>
+    );
+};
