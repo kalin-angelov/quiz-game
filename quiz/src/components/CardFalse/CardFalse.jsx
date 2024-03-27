8c802ebbc7591e7fcc7c0e35e6676daf40fbde1d
Kalizar <kalinangelov@abv.bg>
2024-03-27 10:50:31 +0200

    Crate Card False

---
+++ quiz/src/components/CardFalse/CardFalse.jsx
@@ -0,0 +1,11 @@
+import styles from './CardFalse.module.css';
+
+export const CardFalse = () => {
+    return (
+        <div className={styles.card}>
+            <div className={styles.cardFront}>
+                <img src="images/card-front-false.png" alt="card-front-false" className={styles.cardImg} />
+            </div>
+        </div>
+    );
+};
