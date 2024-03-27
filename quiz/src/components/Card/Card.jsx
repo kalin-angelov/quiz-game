134c42a7526cd7c3c7f141b54c91fe0fb668d341
Kalizar <kalinangelov@abv.bg>
2024-03-27 10:49:25 +0200

    Create Card

---
+++ quiz/src/components/Card/Card.jsx
@@ -0,0 +1,11 @@
+import styles from './Card.module.css';
+
+export const Card = () => {
+    return (
+        <div className={styles.card}>
+            <div className={styles.cardFront}>
+                <img src="images/card-back.png" alt="card-back" className={styles.cardImg} />
+            </div>
+        </div>
+    );
+};
