50c48277b5016de1de36fe44b8ef9c547d28626a
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-09-28 18:35:47 +0300

    Update MainCard.jsx

--- src/components/Cards/MainCard.jsx
+++ src/components/Cards/MainCard.jsx
@@ -3,9 +3,9 @@ import styles from "./scss/Card.module.css";
 export const MainCard = () => {
     return (
         <div className={styles.mainCard}>
-            <div className={styles.cardFront}>
-                <img src="/images/card-back.png" alt="card-back" className={styles.cardImg} />
-            </div>
+            <figure className={styles.cardFront}>
+                <img src="/images/card-back-v2.png" alt="card-back" className={styles.cardImg} />
+            </figure>
         </div>
     );
 };
