327440ab2997ff737f4fcc26af96b0edad173436
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-09-28 18:36:04 +0300

    Update TrueCard.jsx

--- src/components/Cards/TrueCard.jsx
+++ src/components/Cards/TrueCard.jsx
@@ -3,9 +3,9 @@ import styles from "./scss/Card.module.css";
 export const TrueCard = () => {
     return (
         <div className={styles.card}>
-            <div className={styles.cardFront}>
-                <img src="/images/card-front-true.png" alt="card-front-true" className={styles.cardImg} />
-            </div>
+            <figure className={styles.cardFront}>
+                <img src="/images/card-correct-v2.png" alt="card-front-true" className={styles.cardImg} />
+            </figure>
         </div>
     );
 };
